7603acca2dc9f3c0ac60d5c692334a126f007ed0
Murat Babayiğit <babayigit@gmail.com>
2023-10-13 20:22:44 +0300

    first commit

---
+++ src/hilal.java
@@ -0,0 +1,13 @@
+public class hilal {
+    public static void main(String[] args) {
+        System.out.println("Merhaba Github");
+        /*
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/muratbabayigit/hasanyasar.git
+git push -u origin main
+         */
+    }
+}
